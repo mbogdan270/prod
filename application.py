ff0c7c1fe509361ce2ad6080ecab1a722dc58068
System Administrator <root@Bogdans-MacBook-Pro.local>
2019-02-28 15:01:32 +0200

    Commit

--- application.py
+++ application.py
@@ -15,6 +15,8 @@ if __name__ == '__main__':
     port = '4503'
     key = 'xvfT9T4FRCWV=W@v?=+Q}GHL'
     command = "/usr/src/app/push_publisher.sh " + 'http://' + ip + ':' + port + '/crx/packmgr/service.jsp' + ' ' + key
-    #subprocess.call(command, shell=True)
+    p = subprocess.call(command, shell=True)
+    #os.waitpid(p.pid, 0)
+
     port = int(os.environ.get('PORT', 8000))
     app.run(host='0.0.0.0', port=port)
