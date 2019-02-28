8853c410fb844e4a452e9549a389fbcf07224033
System Administrator <root@Bogdans-MacBook-Pro.local>
2019-02-28 15:45:30 +0200

    Commit

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,6 @@ FROM python:2.7
 # Add sample application
 ADD application.py /tmp/application.py
 COPY requirements.txt /usr/src/app/
-COPY ./aem-apps-file/* /usr/src/app/
-COPY ./aem-apps-file/Archive /usr/src/app/Archive
 RUN pip install --no-cache-dir -r /usr/src/app/requirements.txt
 RUN apt-get update
 RUN yes | apt-get install zip unzip
--- application.py
+++ application.py
@@ -11,11 +11,11 @@ def hello():
 
 
 if __name__ == '__main__':
-    ip = '35.183.247.189'
-    port = '4503'
-    key = 'xvfT9T4FRCWV=W@v?=+Q}GHL'
-    command = "/usr/src/app/push_publisher.sh " + 'http://' + ip + ':' + port + '/crx/packmgr/service.jsp' + ' ' + key
-    os.system(command)
+#    ip = '35.183.247.189'
+#    port = '4503'
+#    key = 'xvfT9T4FRCWV=W@v?=+Q}GHL'
+#    command = "/usr/src/app/push_publisher.sh " + 'http://' + ip + ':' + port + '/crx/packmgr/service.jsp' + ' ' + key
+#    os.system(command)
     #p.wait()
     #os.waitpid(p.pid, 0)
 
