80d9fc99ebe9104bb6ed7190be3862ffd9800a72
System Administrator <root@Bogdans-MacBook-Pro.local>
2019-02-28 14:39:27 +0200

    Commit

--- application.py
+++ application.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return redirect("http://35.183.247.189:4503/content/we-retail/us/en.html", code=302)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8000))
